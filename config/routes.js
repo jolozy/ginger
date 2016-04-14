99f2a6e9ff8b9784c0962f8c645dd84c66392376
jolozy <joannelozy@outlook.com>
2016-04-14 21:26:11 +0800

    saves small changes

--- config/routes.js
+++ config/routes.js
@@ -29,10 +29,6 @@ router.route('/api/listings')
 router.route('/admin/listings/')
   .get(listingsController.getAllListings);
 // FOR ADMIN: SUBMISSION
-// router.route('/admin/listings/new')
-//   .get(authorizeUser(), listingsController.adminSubmitForm)
-//   .post(authorizeUser(), listingsController.adminCreateListing);
-
 router.route('/admin/listings/new')
   .get(listingsController.adminSubmitForm)
   .post(authorizeUser(), listingsController.adminCreateListing);
